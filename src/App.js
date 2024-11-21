794b04dd1f2b447f7a198804871e06cc17db23b7
tosi <tototoox4580@gmail.com>
2024-11-21 13:19:45 +0900

    Initial commit with project files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Amplify App</h1>
+        <p>Deploy your React app with AWS Amplify!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
