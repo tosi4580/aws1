92df46c60dc0c20849765def246d326c44d577b1
tosi4580 <tototoox4580@gmail.com>
2024-11-21 17:41:08 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My Amplify App</h1>
+        <h1>Welcome to My Amplify App!!!!!</h1>
         <p>Deploy your React app with AWS Amplify!</p>
       </header>
     </div>
