6ee5d873f723cb3b7360d9d51bae69a1a608d3f8
tosi4580 <tototoox4580@gmail.com>
2024-12-02 17:40:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  render()
   return (
     <div className="App">
       <header className="App-header">
-        <h1>yokoyama</h1>
-        <p>toshiki</p>
+        <h1>aws</h1>
+        <p>web</p>
     <a onClick={this.handleClick}>上り< /a>
       </header>
     </div>
   );
-
+}
 handleClick(); {
     alert('Click!')
   }
-}
+
 export default App;
