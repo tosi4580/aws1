cc2496b8b902faf41446a37c0a7f608fa86a583c
tosi4580 <tototoox4580@gmail.com>
2024-12-02 17:16:05 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
         <h1>yokoyama</h1>
         <p>toshiki</p>
     </a> 
-      <a on Click={this.handleClick1}>上り</a>
+    <a on Click={this.handleClick1}>上り</a>
     </b>
-      <b on Click={this.handleClick2}>下り</b>
+    <b on Click={this.handleClick2}>下り</b>
       </header>
     </div>
   );
-}
+
 handleClick1() {
     alert('Click!')
   }
