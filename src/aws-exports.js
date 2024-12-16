9d4fddb9cbdf1c1b7ca01228cb6e1847164fa00f
tosi4580 <tototoox4580@gmail.com>
2024-12-16 11:25:09 +0900

    Create aws-exports.js

---
+++ src/aws-exports.js
@@ -0,0 +1,14 @@
+const awsmobile = {
+    "aws_project_region": "ap-northeast-1",
+    "aws_cognito_identity_pool_id": "ap-northeast-1:EXAMPLE",
+    "aws_cognito_region": "ap-northeast-1",
+    "aws_user_pools_id": "ap-northeast-1_EXAMPLE",
+    "aws_user_pools_web_client_id": "EXAMPLE",
+    "aws_appsync_graphqlEndpoint": "https://EXAMPLE.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+    "aws_appsync_region": "ap-northeast-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_user_files_s3_bucket": "EXAMPLE",
+    "aws_user_files_s3_bucket_region": "ap-northeast-1"
+};
+
+export default awsmobile;
