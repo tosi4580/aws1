c7549bf69c016d647b0fdf76492d406f68d0857c
tosi4580 <tototoox4580@gmail.com>
2024-12-16 17:51:55 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Amplify, Auth, API } from 'aws-amplify'; 
 import awsconfig from './aws-exports';
@@ -8,14 +8,14 @@ Amplify.configure(awsconfig);
 function App() {
   const [northboundTimetable, setNorthboundTimetable] = useState([]);
   const [southboundTimetable, setSouthboundTimetable] = useState([]);
+  const [loading, setLoading] = useState(false); // ローディング状態を追加
+  const [currentTime, setCurrentTime] = useState(""); // 現在の時刻を保存
 
-  useEffect(() => {
-    fetchTimetable();
-  }, []);
-
-  const fetchTimetable = async () => {
+  const fetchTimetable = async (direction) => {
+    setLoading(true); // ローディング状態を設定
     try {
       const now = new Date();
+      setCurrentTime(now.toLocaleTimeString()); // 現在の時刻を保存
       now.setHours(now.getHours() + 9); 
       now.setMinutes(now.getMinutes() + 10); 
       const timeString = now.toTimeString().substr(0, 5); 
@@ -24,13 +24,15 @@ function App() {
       const response = await fetch(apiUrl);
       const data = await response.json();
       
-      const northbound = data.filter(item => item.railDirection === 'Northbound');
-      const southbound = data.filter(item => item.railDirection === 'Southbound');
-      
-      setNorthboundTimetable(northbound);
-      setSouthboundTimetable(southbound);
+      if (direction === 'Northbound') {
+        setNorthboundTimetable(data.filter(item => item.railDirection === 'Northbound'));
+      } else {
+        setSouthboundTimetable(data.filter(item => item.railDirection === 'Southbound'));
+      }
     } catch (error) {
       console.error('Error fetching timetable:', error);
+    } finally {
+      setLoading(false); // ローディング状態を解除
     }
   };
 
@@ -38,6 +40,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>次の電車</h1>
+        <div>次の電車は {currentTime}</div>
+        <button onClick={() => fetchTimetable('Northbound')} disabled={loading} className="fetch-button">
+          {loading ? '読み込み中...' : '上北台行き'}
+        </button>
+        <button onClick={() => fetchTimetable('Southbound')} disabled={loading} className="fetch-button">
+          {loading ? '読み込み中...' : '多摩センター行き'}
+        </button>
         <div>
           <h2>上北台行き (北行き)</h2>
           <ul>
