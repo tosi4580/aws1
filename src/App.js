c7f67ea5b569ad4e8babf32a60c1cb14e3e41bb2
tosi4580 <tototoox4580@gmail.com>
2024-12-16 19:45:10 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> {currentTime}</h1>
         <h1>乗れる次の電車は...</h1>
         <div> {currentTime}に出れば間に合う ({currentTimePlus10} に出発予定)</div>
         <div className="button-container">
