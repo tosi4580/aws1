8bcac84927544d3d239e21412f0407dcd5b23a30
tosi4580 <tototoox4580@gmail.com>
2024-12-16 11:19:09 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,11 +22,9 @@ function App() {
       const timeString = now.toTimeString().substr(0, 5); // 時間をHH:MM形式に変換
       const apiUrl = `https://xy2igd6s8k.execute-api.ap-northeast-1.amazonaws.com/prod/TimeTable?time=${timeString}`; // ここにAPIのURLを入力
 
-      // API.getの呼び出し部分
       const response = await fetch(apiUrl);
       const data = await response.json();
       
-      // 南行きと北行きに分けて設定
       const northbound = data.filter(item => item.railDirection === 'Northbound');
       const southbound = data.filter(item => item.railDirection === 'Southbound');
       
@@ -42,3 +40,28 @@ function App() {
       <header className="App-header">
         <h1>次の電車</h1>
         <div>
+          <h2>上北台行き (北行き)</h2>
+          <ul>
+            {northboundTimetable.map((item, index) => (
+              <li key={index}>
+                {item.station} - {item.departureTime} - {item.destinationStation}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h2>多摩センター行き (南行き)</h2>
+          <ul>
+            {southboundTimetable.map((item, index) => (
+              <li key={index}>
+                {item.station} - {item.departureTime} - {item.destinationStation}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
